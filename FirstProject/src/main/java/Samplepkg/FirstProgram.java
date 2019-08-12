31451e92032970629ef378edfff573fb8c63e1ae
Sai Deepak <Sai Deepak@Deepak-PC>
2019-08-12 21:32:13 +0530

    test1

--- FirstProject/src/main/java/Samplepkg/FirstProgram.java
+++ FirstProject/src/main/java/Samplepkg/FirstProgram.java
@@ -3,6 +3,7 @@ package Samplepkg;
 public class FirstProgram {
 public static void main(String[] args) {
 	System.out.println("Welcome to github");
+	System.out.println("Welcome to github");
 	
 	
 }
